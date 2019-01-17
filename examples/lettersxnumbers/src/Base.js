b2193f18aa0d8b8c973b001f8fc6a4097dc213d8
Maurice Defaux <defauxmaurice@yahoo.com>
2019-01-17 17:25:36 +0100

    Extended number of rows and columns in example lettersxnumbers.

--- examples/lettersxnumbers/src/Base.js
+++ examples/lettersxnumbers/src/Base.js
@@ -3,8 +3,8 @@ import ForteTable from "forte-table";
 
 export default ({ name }) => (
   <ForteTable
-    rows={() => [1, 2, 3]}
-    columns={() => ["a", "b", "c"]}
+    rows={() => [1, 2, 3, 4, 5, 6, 7, 8, 9]}
+    columns={() => ["a", "b", "c", "d", "e", "f", "g", "h", "i"]}
     cellRender={(c, r) => {
       return "cell " + c + r;
     }}
