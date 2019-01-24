bdb5f20326fc8663fdc43619724fbb9638170ed1
Maurice Defaux <defauxmaurice@yahoo.com>
2019-01-24 18:08:52 +0100

    Extended example columns and row.

--- examples/src/App.js
+++ examples/src/App.js
@@ -5,19 +5,22 @@ import './App.css';
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <h1 className="App-title"> {"\u2728"} Forte Table {"\u2728"}</h1>
+      <h1 className="App-title">
+        {' '}
+        {'\u2728'} Forte Table {'\u2728'}
+      </h1>
     </header>
     <ForteTable
-      rows={() => [1, 2, 3]}
-      columns={() => ["a", "b", "c"]}
+      rows={() => [1, 2, 3, 4, 5, 6, 7, 8, 9]}
+      columns={() => ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']}
       cellRender={(c, r) => {
-        return "cell" + c + r;
+        return 'cell' + c + r;
       }}
       onCellClick={(e, c, r) => {
-        alert("buuu:" + c + r);
+        alert('buuu:' + c + r);
       }}
     />
   </div>
-)
+);
 
 export default App;
